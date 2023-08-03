f69b62a63188cc7e4f4abec0675813aef5670cfc
Dallas Smart <dsmart10@outlook.com>
2023-08-03 11:38:47 +1000

    Initial scene setup commit

---
+++ YOUR NOT ALONE - Game Project/Assets/Scripts/Player/Player.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float health;
+    public float movementSpeed;
+    public float grabRange;
+    [SerializeField] private GameObject grabbedObject;
+    public float throwRange;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void Move()
+    {
+        
+    }
+
+    private void Grab()
+    {
+
+    }
+
+    private void Throw()
+    {
+
+    }
+
+    private void AddHealth()
+    {
+
+    }
+
+    private void Death()
+    {
+
+    }
+
+    private void Interact()
+    {
+
+    }
+}
